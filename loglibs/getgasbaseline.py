fa8ecb3364db00995e2a6c1cb4bf667abbe554f6
Tomasz <garbatygrabarz@gmail.com>
2021-05-29 23:02:13 +0200

    Create getgasbaseline.py

---
+++ loglibs/getgasbaseline.py
@@ -0,0 +1,47 @@
+import bme680
+import time
+
+
+try:
+    sensor = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
+except (RuntimeError, IOError):
+    sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
+
+# These oversampling settings can be tweaked to
+# change the balance between accuracy and noise in
+# the data.
+
+sensor.set_humidity_oversample(bme680.OS_2X)
+sensor.set_pressure_oversample(bme680.OS_4X)
+sensor.set_temperature_oversample(bme680.OS_8X)
+sensor.set_filter(bme680.FILTER_SIZE_3)
+sensor.set_gas_status(bme680.ENABLE_GAS_MEAS)
+
+sensor.set_gas_heater_temperature(320)
+sensor.set_gas_heater_duration(150)
+sensor.select_gas_heater_profile(0)
+
+# start_time and curr_time ensure that the
+# burn_in_time (in seconds) is kept track of.
+
+start_time = time.time()
+curr_time = time.time()
+burn_in_time = 300
+
+burn_in_data = []
+
+# Collect gas resistance burn-in values, then use the average
+# of the last 50 values to set the upper limit for calculating
+# gas_baseline.
+print('Collecting gas resistance burn-in data for 5 mins\n')
+while curr_time - start_time < burn_in_time:
+    curr_time = time.time()
+    if sensor.get_sensor_data() and sensor.data.heat_stable:
+        gas = sensor.data.gas_resistance
+        burn_in_data.append(gas)
+        print('Gas: {0} Ohms'.format(gas))
+        time.sleep(1)
+
+gas_baseline = sum(burn_in_data[-50:]) / 50.0
+with open('config.py', 'a') as file:
+    file.write(f'GAS_BASE = {gas_baseline}')
